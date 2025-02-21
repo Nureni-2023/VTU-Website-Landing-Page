dbffbce59ff2559e5eb2313d1ea88c7ee5c80b1f
ADISA NURENI OLANIYI <143888367+Nureni-2023@users.noreply.github.com>
2025-02-21 05:50:30 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  hamburger.classList.toggle('active');
+});
+
+
+// ///////////////////////////////////////////////////////////
+// // Set current year
+// const yearEl = document.querySelector(".year");
+// const currentYear = new Date().getFullYear();
+// yearEl.textContent = currentYear;
+
+
+// ///////////////////////////////////////////////////////////
+// // Smooth scrolling animation
+
+// const allLinks = document.querySelectorAll("a:link");
+
+// allLinks.forEach(function (link) {
+//   link.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const href = link.getAttribute("href");
+
+//     // Scroll back to top
+//     if (href === "#")
+//       window.scrollTo({
+//         top: 0,
+//         behavior: "smooth",
+//       });
+
+//     // Scroll to other links
+//     if (href !== "#" && href.startsWith("#")) {
+//       const sectionEl = document.querySelector(href);
+//       sectionEl.scrollIntoView({ behavior: "smooth" });
+//     }
+
+// //     // Close mobile naviagtion
+// //     if (link.classList.contains("main-nav-link"))
+// //       headerEl.classList.toggle("nav-open");
+// //   });
+// // });
+
+// ///////////////////////////////////////////////////////////
+// // Sticky navigation
+
+// const sectionHeroEl = document.querySelector(".header");
+
+// const obs = new IntersectionObserver(
+//   function (entries) {
+//     const ent = entries[0];
+//     console.log(ent);
+
+//     if (ent.isIntersecting === false) {
+//       document.body.classList.add("sticky");
+//     }
+
+//     if (ent.isIntersecting === true) {
+//       document.body.classList.remove("sticky");
+//     }
+//   },
+// );
+
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".header");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+
+///////////////////////////////////////////////////////////
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
